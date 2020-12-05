cee1036c87856d67484f367fadda299e2b4e1e13
AkiaCode <catry.me@gmail.com>
2020-12-05 18:54:55 +0900

    v2.0

--- test/index.js
+++ test/index.js
@@ -1,15 +1,4 @@
 const test = require('../src/index')
-const client = new test()
 Promise.all([
-    client.capsules(),
-    client.company(),
-    client.crew(),
-    client.dragons(),
-    client.landpads(),
-    client.launches(),
-    client.payloads(),
-    client.roadster(),
-    client.rockets(),
-    client.ships(),
-    client.starlink()
+    test.getData("launches", "past")
 ]).then(console.log)
